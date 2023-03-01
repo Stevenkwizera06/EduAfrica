74b3cab35704a44b3a24c7eef1445f800f345839
Ngabonziza Nestor <ngabonest@gmail.com>
2023-03-01 18:49:31 +0200

    fixing navbar

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -5,59 +5,62 @@ function AboutUs() {
   return (
     <div>
       {" "}
-      <div className="grid lg:grid-cols-2 justify-center items-center max-w-[1024px] mx-auto space-x-20">
-        <div className="flex flex-col items-center gap-8 text-center">
-          <h1 className="text-3xl font-bold">Capital campaign program</h1>
-          <p className="text-start">
-            The Children’s Foundation is working to secure our future in Surrey.
-            Help us make sure that we are there for children and families for
-            years to come. Owning our facility out right will provide greater
-            predictability and control over our occupancy costs and help us
-            focus resources and energy on the services children, families, and
-            community need.
-          </p>
-          <p>
-            We need your help and support to raise $5,000,000 in the next 5
-            years.
-          </p>
+      <Wrapper>
+        {" "}
+        <div className="grid lg:grid-cols-2 lg:p-16 gap-8">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-3xl font-bold">Capital campaign program</h1>
+            <p className="text-start">
+              The Children’s Foundation is working to secure our future in
+              Surrey. Help us make sure that we are there for children and
+              families for years to come. Owning our facility out right will
+              provide greater predictability and control over our occupancy
+              costs and help us focus resources and energy on the services
+              children, families, and community need.
+            </p>
+            <p>
+              We need your help and support to raise $5,000,000 in the next 5
+              years.
+            </p>
+          </div>
+          <div>
+            <img
+              className="object-cover w-full lg:h-[293px]"
+              alt=""
+              src="https://childrens-foundation.org/wp-content/uploads/2022/11/TRW_6928-scaled.jpeg"
+            />
+          </div>
         </div>
-<div>
-        <img
-          className="object-cover   w-[478px] h-[293px]"
-          alt=""
-          src="https://childrens-foundation.org/wp-content/uploads/2022/11/TRW_6928-scaled.jpeg"
-        />
-        </div>
-      </div>
-        <Wrapper>
-      <div className="grid lg:grid-cols-2 p-16 gap-8">
-        <img
-          className="object-contain"
+      </Wrapper>
+      <Wrapper>
+        <div className="grid lg:grid-cols-2 lg:p-16 gap-8">
+          <img
+            className="object-contain order-2 lg:order-1"
             src="https://childrens-foundation.org/wp-content/uploads/2022/11/TRW_7098-scaled.jpeg"
             alt=""
           />
-        <div className="flex flex-col gap-4 ">
-          <h1 className="flex  text-3xl font-bold">What we do</h1>
-          <p className="max-w-[470px] h-[192px]">
-            The Children’s Foundation provides specialized treatment,
-            counselling and attentive care to children, youth, and families with
-            complex challenges through a variety of programs and services. Some
-            of the challenges our clients face ranges from ASD, FASD, severe
-            social, emotional, and developmental disorders and disabilities, and
-            challenges that stem from a history of trauma, among many others.
-          </p>
-          <div className="flex  ">
-            <a
-              className="px-8 text-white font-semibold py-4 bg-pink-500"
-              href=""
-            >
-              DONATE
-            </a>
+          <div className="flex flex-col gap-4 order-1 lg:order-2">
+            <h1 className="flex  text-3xl font-bold">What we do</h1>
+            <p className="max-w-[470px] min-h-[192px]">
+              The Children’s Foundation provides specialized treatment,
+              counselling and attentive care to children, youth, and families
+              with complex challenges through a variety of programs and
+              services. Some of the challenges our clients face ranges from ASD,
+              FASD, severe social, emotional, and developmental disorders and
+              disabilities, and challenges that stem from a history of trauma,
+              among many others.
+            </p>
+            <div className="flex  ">
+              <a
+                className="px-8 text-white font-semibold py-4 bg-pink-500"
+                href=""
+              >
+                DONATE
+              </a>
+            </div>
           </div>
         </div>
-          
-      </div>
-        </Wrapper>
+      </Wrapper>
       <Wrapper>
         <h1 className="text-[30px] font-[700] leading-[36px] text-[#2e2e2e] max-w-[980px] px-[10px]  text-center">
           The Children’s Foundation has helped thousands of children since 1957
@@ -74,20 +77,24 @@ function AboutUs() {
           families. Since that time, The Children’s Foundation has helped
           thousands of vulnerable children and their families to better lives.
         </p>
-        <div className="grid lg:grid-cols-2 mt-4">
+        <div className="grid lg:grid-cols-2 mt-4 ">
           <img
-            className="object-cover"
+            className="object-cover h-full"
             alt=""
             src="https://childrens-foundation.org/wp-content/uploads/2022/11/TRW_6977-scaled.jpeg"
           />
           <div className="flex flex-col items-center text-center gap-6 bg-[#ecdc01] p-[30px]">
-            <h1 className="text-5xl text-[#2e2e2e] text-[30px] font-[700] leading-[36px] text-center py-2.5">Our mission</h1>
+            <h1 className="text-5xl text-[#2e2e2e] text-[30px] font-[700] leading-[36px] text-center py-2.5">
+              Our mission
+            </h1>
             <p className="text-[#707070] max-w-[480px] leading-[28.8px] font-[500]">
               Improving lives by helping children and their families navigate
-              complex challenges. 
+              complex challenges.
             </p>
             <div className="w-[70%] h-[1px] bg-[#2e2e2e]" />
-            <h1 className="text-5xl text-[#2e2e2e] text-[30px] font-[700] leading-[36px] text-center">Our vision</h1>
+            <h1 className="text-5xl text-[#2e2e2e] text-[30px] font-[700] leading-[36px] text-center">
+              Our vision
+            </h1>
             <p className="text-[#707070] max-w-[480px] leading-[28.8px] font-[500]">
               Communities in which each child has the opportunity for optimum
               growth and development, and each family is able to provide the
@@ -96,76 +103,74 @@ function AboutUs() {
           </div>
         </div>
       </Wrapper>
-      <div className=" bg-[#2e8bc4] mt-8 -mb-8 h-[600px]">
-        <div className="max-w-[1024px] mx-auto grid lg:grid-cols-2 justify-center items-center text-center">
+      <Wrapper>
+        <div className=" bg-[#2e8bc4] mt-8 -mb-8 h-[600px]">
+          <div className="max-w-[1080px] mx-auto grid lg:grid-cols-2 gap-8 justify-center items-center">
+            <img
+              className="mt-12"
+              src="https://childrens-foundation.org/wp-content/uploads/2022/11/childrens-foundation_logo-01-1.png"
+              alt=""
+            />
+            <div className="text-white flex flex-col gap-6 items-start   mt-28 ">
+              <h1 className="text-3xl font-bold">
+                Get updates straight to your inbox
+              </h1>
+              <p className="self-start">
+                Sign up to get updates and stay current on what’s happening at
+                the Children’s Foundation.
+              </p>
+              <p className="text-lg font-semibold ">Email Address *</p>
+              <input
+                className="w-[90%] p-2 placeholder:text-white bg-transparent placeholder:text-lg border-2 border-gray-200 focus:outline-none"
+                placeholder="what is your email"
+              />
+              <div className="flex justify-center ">
+                <a
+                  className="text-white  bg-[#B42886] border-[1.6px] text-[20px] font-[500] leading-[32px] px-[35px] mr-5px] mb-[10px] border-none h-[50px] inline-flex items-center justify-center"
+                  href=""
+                >
+                  Subscribe
+                </a>
+              </div>
+              <p className="text-[14px] font-[300] leading-[28.8px]">
+                By submitting this form you are granting The Children’s
+                Foundation permission to email you. You may unsubscribe via the
+                link found at the bottom of every email.
+              </p>
+            </div>
+          </div>
+        </div>
+      </Wrapper>
+      <Wrapper>
+        <div className=" relative">
           <img
-            className="mt-12"
-            src="https://childrens-foundation.org/wp-content/uploads/2022/11/childrens-foundation_logo-01-1.png"
+            className="object-cover max-w-full lg:w-full h-11/12  relative"
+            src="https://childrens-foundation.org/wp-content/uploads/2022/11/TRW_7027-scaled.jpeg"
             alt=""
           />
-          <div className="text-white flex flex-col gap-6 items-start justify-center text-center mt-28 px-6">
-            <h1 className="text-3xl font-bold self-center text-center max-w-[80%]">
-              Get updates straight to your inbox
-            </h1>
-            <p className="self-center text-center">
-              Sign up to get updates and stay current on what’s happening at the
-              Children’s Foundation.
-            </p>
-            <p className="text-lg font-semibold self-center text-center">
-              Email Address *
-            </p>
-            <input
-              className="w-[90%] p-2 placeholder:text-white bg-transparent placeholder:text-lg border-2 border-gray-200 border-2 focus:outline-none"
-              placeholder="what is your email"
-            />
-            <div className="flex justify-center ">
-              <a
-                className="text-white  bg-[#B42886] border-[1.6px] text-[20px] font-[500] leading-[32px] px-[35px] mr-5px] mb-[10px] border-none h-[50px] inline-flex items-center justify-center"
-                href=""
-              >
-                Subscribe
-              </a>
-            </div>
-            <p className="self-center text-center">
-              By submitting this form you are granting The Children’s Foundation
-              permission to email you. You may unsubscribe via the link found at
-              the bottom of every email.
-            </p>
-          </div>
-        </div>
-      </div>
-      
-      <div className=" relative">
-        <img
-          className="object-cover max-w-full lg:w-full h-11/12  relative"
-          src="https://childrens-foundation.org/wp-content/uploads/2022/11/TRW_7027-scaled.jpeg"
-          alt=""
-        />
 
-        <div className="flex justify-center mb-8">
-          <div
-            className="px-8  text-white font-semibold -mt-24 text-center py-8 text-xl bg-[#072951] w-[504px] h-[238px] relative z-40"
-            href=""
-          >
-            <div className="flex flex-col">
-              <p>
-                We improve the lives of children and families with your support
-              </p>
-              <a
-                className="px-8 w-36 justify-center ml-36 mt-10  text-white font-semibold py-4 bg-[#B42886]"
-                href=""
-              >
-                DONATE
-              </a>
+          <div className="flex justify-center mb-8">
+            <div
+              className="px-8  text-white font-semibold -mt-24 text-center py-8 text-xl bg-[#072951] w-[504px] h-[238px] relative z-40"
+              href=""
+            >
+              <div className="flex flex-col">
+                <p>
+                  We improve the lives of children and families with your
+                  support
+                </p>
+                <a
+                  className="px-8 w-36 justify-center ml-36 mt-10  text-white font-semibold py-4 bg-[#B42886]"
+                  href=""
+                >
+                  DONATE
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Wrapper>
     </div>
-
-
-
-
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import items from "@/data/menuItems";
 import DropDown from "./DropDown";
 import { FiMenu } from "react-icons/fi";
+import Wrapper from "./Wrapper";
 
 function Navbar() {
   const [activeTab, setActiveTab] = useState();
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <div className="lg:h-screen relative">
+    <div className="w-full relative">
       <video
         loop
         autoPlay
@@ -16,131 +17,134 @@ function Navbar() {
         className="absolute object-cover top-0 left-0 w-full h-[90%]"
         src="https://childrens-foundation.org/wp-content/uploads/2022/11/BANNER_Compressed.mp4"
       ></video>
-
-      <div className="relative max-w-[1426px] mx-auto flex flex-col justify-items-start justify-between h-[90%] pt-6 lg:px-0 ">
-        <div className="flex items-center w-full justify-between mx-auto py-6">
-          <a href="">
-            <img
-              className="h-[45px] lg:h-[61px] "
-              src="https://childrens-foundation.org/wp-content/uploads/2022/11/childrens-foundation_logo_white_letters.png"
-              alt=""
-            />
-          </a>
-          <div className="group w-fit md:flex justify-center ">
-            <a
-              className="px-10 hidden md:flex text-white cursor-pointer font-semibold py-4 bg-darkBlue "
-              href="#"
-            >
-              Menu
+      <Wrapper>
+        <div className="relative mx-auto flex flex-col justify-items-start justify-between  pt-6 lg:px-0 ">
+          <div className="flex items-center w-full justify-between mx-auto py-6">
+            <a href="">
+              <img
+                className="h-[45px] lg:h-[61px] "
+                src="https://childrens-foundation.org/wp-content/uploads/2022/11/childrens-foundation_logo_white_letters.png"
+                alt=""
+              />
             </a>
-            <FiMenu
-              className="md:hidden"
-              size={30}
-              color="fff"
-              onClick={() => setShowMenu(!showMenu)}
-            />
+            <div className="group w-fit md:flex justify-center ">
+              <a
+                className="px-10 hidden md:flex text-white cursor-pointer font-semibold py-4 bg-darkBlue "
+                href="#"
+              >
+                Menu
+              </a>
+              <FiMenu
+                className="md:hidden"
+                size={30}
+                color="fff"
+                onClick={() => setShowMenu(!showMenu)}
+              />
 
-            <div
-              className="hidden absolute md:group-hover:block mt-14"
-              onMouseLeave={() => setActiveTab()}
-            >
-              <div className="flex">
-                {items.map((item, index) => (
-                  <ul
-                    key={index}
-                    onMouseEnter={() => setActiveTab(item.title)}
-                    className="flex text-white font-medium text-sm"
-                  >
-                    <li
-                      className={`${item.bgColor} w-full px-20 py-4 hover:cursor-default`}
+              <div
+                className="hidden absolute md:group-hover:block mt-14"
+                onMouseLeave={() => setActiveTab()}
+              >
+                <div className="flex">
+                  {items.map((item, index) => (
+                    <ul
+                      key={index}
+                      onMouseEnter={() => setActiveTab(item.title)}
+                      className="flex text-white font-medium text-sm"
                     >
-                      <a href="#" className="hover:text-gray-400">
-                        {item.title}
-                      </a>
-                    </li>
-                  </ul>
-                ))}
-              </div>
-              <div>
-                {items.map((item, index) => (
-                  <div
-                    key={index}
-                    style={{
-                      backgroundColor: item.bgColor,
-                    }}
-                    className={`flex ${
-                      item.title === "Learn More" ? "text-black" : "text-white"
-                    } font-semibold`}
-                  >
-                    {activeTab === item.title
-                      ? item.subItems.map((subItem, index) => (
-                          <div key={index} className={`${item.bgColor} w-full`}>
-                            {subItem.title === "" ? (
-                              <br />
-                            ) : (
-                              <a href={subItem.title.href}>
-                                {subItem.title.name}
-                              </a>
-                            )}
-                            {subItem.contents.map((content, index) => (
-                              <div key={index}>
-                                <a href={content.href}>{content.name}</a>
-                              </div>
-                            ))}
-                          </div>
-                        ))
-                      : ""}
-                  </div>
-                ))}
+                      <li
+                        className={`${item.bgColor} w-full px-20 py-4 hover:cursor-default`}
+                      >
+                        <a href="#" className="hover:text-gray-400">
+                          {item.title}
+                        </a>
+                      </li>
+                    </ul>
+                  ))}
+                </div>
+                <div>
+                  {items.map((item, index) => (
+                    <div
+                      key={index}
+                      style={{
+                        backgroundColor: item.bgColor,
+                      }}
+                      className={`flex ${
+                        item.title === "Learn More"
+                          ? "text-black"
+                          : "text-white"
+                      } font-semibold`}
+                    >
+                      {activeTab === item.title
+                        ? item.subItems.map((subItem, index) => (
+                            <div
+                              key={index}
+                              className={`${item.bgColor} w-full`}
+                            >
+                              {subItem.title === "" ? (
+                                <br />
+                              ) : (
+                                <a href={subItem.title.href}>
+                                  {subItem.title.name}
+                                </a>
+                              )}
+                              {subItem.contents.map((content, index) => (
+                                <div key={index}>
+                                  <a href={content.href}>{content.name}</a>
+                                </div>
+                              ))}
+                            </div>
+                          ))
+                        : ""}
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
+
+            <DropDown />
+
+            <a
+              className="bg-pink px-4 lg:px-8 py-2 lg:py-4 w-fit text-center text-white font-semibold "
+              href=""
+            >
+              Donate
+            </a>
           </div>
-          
-        <DropDown />
+          <DropDown showMenu={showMenu} />
 
-          <a
-            className="bg-pink px-4 lg:px-8 py-2 lg:py-4 w-fit text-center text-white font-semibold "
-            href=""
-          >
-            Donate
-          </a>
+          <div className="flex items-center justify-start md:w-[1229px]  mx-auto">
+            <h4 className="font-[700] lg:font-bold text-white text-[40px] leading-[36px] lg:text-[100px] lg:leading-[120px]">
+              CHANGING <br /> LIVES <br />
+              <span className="tracking">
+                EVERY <span className="text-brightYellow ">DAY</span>
+              </span>
+            </h4>
+          </div>
+          <div className="max-w-[1080px] mx-auto w-full gap-12 mt-8 invisible md:visible  md:grid lg:grid-cols-3 ">
+            <a
+              className="px-14 text-white font-semibold text-center py-8 text-2xl bg-brightYellow "
+              href=""
+            >
+              Ways to give
+            </a>
+
+            <a
+              className="px-14 text-white font-semibold  text-center py-8 text-2xl bg-darkBlue "
+              href=""
+            >
+              Our programme
+            </a>
+
+            <a
+              className="px-14 text-white font-semibold  text-center py-8 text-2xl bg-pink "
+              href=""
+            >
+              Stories
+            </a>
+          </div>
         </div>
-        <DropDown
-          showMenu={showMenu}
-        />
-
-        <div className="flex items-center justify-start md:w-[1229px]  mx-auto">
-          <h4 className="font-[700] lg:font-bold text-white text-[40px] leading-[36px] lg:text-[100px] lg:leading-[120px]">
-            CHANGING <br /> LIVES <br />
-            <span className="tracking">
-              EVERY <span className="text-brightYellow ">DAY</span>
-            </span>
-          </h4>
-        </div>
-        <div className="max-w-[1080px] mx-auto w-full gap-12 mt-8 grid lg:grid-cols-3 ">
-          <a
-            className="px-14 text-white font-semibold text-center py-8 text-2xl bg-brightYellow "
-            href=""
-          >
-            Ways to give
-          </a>
-
-          <a
-            className="px-14 text-white font-semibold  text-center py-8 text-2xl bg-darkBlue "
-            href=""
-          >
-            Our programme
-          </a>
-
-          <a
-            className="px-14 text-white font-semibold  text-center py-8 text-2xl bg-pink "
-            href=""
-          >
-            Stories
-          </a>
-        </div>
-      </div>
-      
+      </Wrapper>
     </div>
   );
 }
